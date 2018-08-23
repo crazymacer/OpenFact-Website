7acdc074c8ce6ae2e4c64209784b99b05973b84d
crazymacer <mario_ct5@hotmail.com>
2018-08-23 12:39:12 -0500

    Change buttons name

--- index.php
+++ index.php
@@ -70,9 +70,9 @@
         <div class="container mt-5">
             <h1 class="logo-text">OpenFact</h1>
             <p class="tagline">Sistema de Facturación Electrónica. </p>
-            <div class="btn-group">
-                <a href="http://openfact-innpath.ddns.me/" target="_blank" class="btn btn-outline-light">Openfact West</a>
-                <a href="http://Openfact.dnsfor.me " target="_blank" class="btn btn-outline-light">Openfact North</a>
+            <div class="btn-group" style="margin-bottom:5px;">
+                <a href="http://openfact-innpath.ddns.me/" target="_blank" class="btn btn-outline-light">Openfact Sur</a>
+                <a href="http://Openfact.dnsfor.me " target="_blank" class="btn btn-outline-light">Openfact Centro</a>
             </div>
             <p>
                 <a href="http://openfact-test.ddns.me/" target="_blank" class="btn btn-outline-light my-3 my-sm-0 ml-lg-3">VER DEMOSTRACIÓN</a>
